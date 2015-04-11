58e36f0ae553aa75b20a049d303d45fef0cf3047
Kedarnag Mukanahallipatna <kedarnag@ACC8867D.ipt.aol.com>
2015-04-11 22:14:36 +0530

    3rd Commit

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 # Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+# ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+require File.expand_path(__FILE__)+ '/../../config/boot'
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
