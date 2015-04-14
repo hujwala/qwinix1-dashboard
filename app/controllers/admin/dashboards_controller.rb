b333d9f26acf65afa6b2128d65e8eea8b13a4067
Kedarnag Mukanahallipatna <kedarnag@Kedarnags-MacBook-Pro.local>
2015-04-14 10:52:15 +0530

    [master] Adds the Github Widget and makes changes to dashboard controller.

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,62 +1,66 @@
 class Admin::DashboardsController < ApplicationController
 
-	def index
+  def index
     get_collections
     dashboard_widgets = DashboardWidget.where(dashboard_id: @dashboard.id)
     dashboard_widgets.each do |dw|
-    # @dashboard_widget = dw  if  dw.access_token?
-		$dashboard_widget = dw	#if 	dw.code_api_token.present? && dw.code_repo_id.present?
+      if dw.access_token.present? && dw.organization_name.present? && dw.repo_name.present?
+        $dashboard_widget_github = dw
+      elsif dw.code_repo_id.present? && dw.code_api_token.present?
+        $dashboard_widget_code = dw
+      else
+        $dashboard_widget_jira = dw
+      end
+    end
+  end
 
-	end
-	end
+  def new
+    @dashboard = Dashboard.new
+  end
 
-	def new
-		@dashboard = Dashboard.new
-	end
+  def create
+    @dashboard = Dashboard.new(dashboard_params)
+    if @dashboard.valid?
+      @dashboard.save
+      @success = true
+    else
+      @success = false
+    end
+  end
 
-	def create
-		@dashboard = Dashboard.new(dashboard_params)
-		if @dashboard.valid?
-			@dashboard.save
-			@success = true
-		else
-			@success = false
-		end
-	end
+  def edit
+    @dashboard = Dashboard.find(params[:id])
+  end
 
-	def edit
-		@dashboard = Dashboard.find(params[:id])
-	end
+  def show
+    @dashboard = Dashboard.find(params[:dashboard_id])
+  end
 
-	def show
-		@dashboard = Dashboard.find(params[:dashboard_id])
-	end
+  def update
+    @dashboard = Dashboard.find(params[:id])
+    if @dashboard.valid?
+      @dashboard.update_attributes(dashboard_params)
+      @success = true
+    else
+      @success = false
+    end
+  end
 
-	def update
-		@dashboard = Dashboard.find(params[:id])
-		if @dashboard.valid?
-			@dashboard.update_attributes(dashboard_params)
-			@success = true
-		else
-			@success = false
-		end
-	end
+  private
 
-	private
+  def dashboard_params
+    params[:dashboard].permit(:name)
+  end
 
-	def dashboard_params
-		params[:dashboard].permit(:name)
-	end
-
-	def get_collections
+  def get_collections
 
     # Fetching the dashboard
     relation = Dashboard.where("")
     @filters = {}
 
     if params[:query]
-    	@query = params[:query].strip
-    	relation = relation.search(@query) if !@query.blank?
+      @query = params[:query].strip
+      relation = relation.search(@query) if !@query.blank?
     end
 
     @dashboards = relation.order("created_at desc")
--- app/jobs/github_prs.rb
+++ app/jobs/github_prs.rb
@@ -1,21 +1,23 @@
-# require 'octokit'
+require 'octokit'
 
-# Dashing.scheduler.every '1m', :first_in => 0 do |job|
-#   client = Octokit::Client.new(:access_token => "b1d3dfeb056ff5df332e08336910a29e2883923e")
-#   my_organization = "Qwinix"
-#   repos = client.organization_repositories(my_organization).map { |repo| repo.name }
 
-#   open_pull_requests = repos.inject([]) { |pulls, repo|
-#     client.pull_requests("#{my_organization}/#{$dashboard_widget.repo_name}", :state => 'close').each do |pull|
-#       pulls.push({
-#         title: pull.title,
-#         repo: $dashboard_widget.repo_name,
-#         updated_at: pull.updated_at.strftime("%b %-d %Y, %l:%m %p"),
-#         creator: "@" + pull.user.login,
-#         })
-#     end
-#     pulls
-#   }
+Dashing.scheduler.every '10s', :first_in => 0 do |job|
+  unless $dashboard_widget_github.nil?
+    client = Octokit::Client.new(:access_token => $dashboard_widget_github.access_token)
+    my_organization = $dashboard_widget_github.organization_name
+    repos = client.organization_repositories(my_organization).map { |repo| repo.name }
 
-#   Dashing.send_event('AuthToken', { header: "Open Pull Requests", pulls: open_pull_requests })
-# end
+    open_pull_requests = repos.inject([]) { |pulls, repo|
+      client.pull_requests("#{my_organization}/#{$dashboard_widget_github.repo_name}", :state => 'open').each do |pull|
+        pulls.push({
+          title: pull.title,
+          repo: $dashboard_widget_github.repo_name,
+          updated_at: pull.updated_at.strftime("%b %-d %Y, %l:%m %p"),
+          creator: "@" + pull.user.login,
+          })
+      end
+      pulls
+    }
+  Dashing.send_event('AuthToken', { header: "Open Pull Requests", pulls: open_pull_requests })
+  end
+end
