608cb45e1b86849f11d9c9b3c7ee433a0e3a1eb9
Kavyahs <kshanthegowda@qwinixtech.com>
2015-04-20 14:36:26 +0530

    typo error fixed

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,10 +1,6 @@
 class Admin::SessionsController < ApplicationController
   before_filter :sign_out, only:[:index]
 
-  def index
-    @user = User.new
-  end
-
   def create
     @user = User.authenticate(params[:email], params[:password])
     if @user
