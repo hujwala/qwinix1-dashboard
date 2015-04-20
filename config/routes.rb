b8fb6d400fe07ace362207f58596a00fc0cb462a
Kavyahs <kshanthegowda@qwinixtech.com>
2015-04-20 19:10:37 +0530

    UI of widget file and updated validation file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount Dashing::Engine, at: Dashing.config.engine_path
 
   root :to => 'admin/sessions#index'
-  get 'users/unique_email' => 'users#unique_email'
+  get 'users/unique_email' => 'admin/users#unique_email'
   # get "log_in" => "users#new", :as => "log_in"
 
   # Sign In URLs for users
