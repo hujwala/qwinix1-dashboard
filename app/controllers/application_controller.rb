70874ca368390d67fde00d8cc94978a20edf9d7a
asimha <asimha@qwinixtech.com>
2015-04-24 15:19:43 +0530

    Add controllers from gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include ApplicationHelper
   protect_from_forgery with: :exception
-  helper :all
   helper_method :current_user
-private
+
+  private
   def current_user
     @current_user ||= User.find_by_id(session[:user_id])
   end
