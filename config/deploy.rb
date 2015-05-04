80431f88221b130fc01c67d999584610236f7c2a
asimha <asimha@qwinixtech.com>
2015-05-04 15:34:01 +0530

    updated cap file with puma config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,16 @@
 set :application, 'Dashingproject'
 set :repo_url, 'https://github.com/Qwinix/dashingproject.git'
 set :scm, :git
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false  # Change to true if using ActiveRecord
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 set :format, :pretty
@@ -87,4 +97,14 @@ namespace :assets do
       end
     end
   end
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
 
+  before :start, :make_dirs
+end
